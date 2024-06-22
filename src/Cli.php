1c4a2effd5a2aa20df53c39db0975a2fbde9d95f
cm-ryffel <dimanlancev0@gmail.com>
2024-06-23 00:02:07 +0300

    add cli.php, add cli-php-tools lib

---
+++ src/Cli.php
@@ -0,0 +1,12 @@
+<?php 
+namespace Src\Cli;
+require_once __DIR__ ."/../vendor/autoload.php";
+use function cli\line;
+use function cli\prompt;
+
+function greetings(){
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+
+}
