28fa7c38fcaababf4889e3a755aaf2e8ff61b46e
cm-ryffel <dimanlancev0@gmail.com>
2024-06-23 00:43:12 +0300

    install phpcs, using standard=PSR12

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,14 @@
-<?php 
+<?php
+
 namespace Src\Cli;
-require_once __DIR__ ."/../vendor/autoload.php";
+
+require_once __DIR__ . "/../vendor/autoload.php";
 use function cli\line;
 use function cli\prompt;
 
-function greetings(){
+function greetings()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-
 }
